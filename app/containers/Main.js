684df8cd886165af2f52ef7b82cea5d7ad032ca6
Iggy <igoririanto@rocketmail.com>
2017-01-23 18:03:52 -0800

    reuse getcitycontainer on header

--- app/containers/Main.js
+++ app/containers/Main.js
@@ -1,10 +1,19 @@
 var React = require('react');
-//var GetCityContainer = require('./GetCityContainer');
+var GetCityContainer = require('./GetCityContainer');
 
 var styles = {
   container: {
     width: '100%',
-    height: '100%'
+    height: '92%'
+  },
+  header: {
+    background: '#FFD070',
+    display: 'flex',
+    backgroundSize: 'cover',
+    margin: 0,
+    color: 'white',
+    alignItems: 'center',
+    justifyContent: 'space-between'
   }
 }
 
@@ -12,7 +21,10 @@ var Main = React.createClass({
   render: function () {
     return (
       <div style={styles.container}>
-        <h1>Main.js Header</h1>
+        <div style={styles.header}>
+          <h1>Main.js Header</h1>
+          <GetCityContainer direction={'row'}/>
+        </div>
         {this.props.children}
       </div>
     )
