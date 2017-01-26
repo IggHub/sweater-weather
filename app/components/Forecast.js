044a13173efb0aee85252a58a90f575fc8a48450
Iggy <igoririanto@rocketmail.com>
2017-01-26 11:38:53 -0800

    add helpers/utility

--- app/components/Forecast.js
+++ app/components/Forecast.js
@@ -2,7 +2,7 @@ var React = require('react');
 var Loading = require('./Loading');
 
 function Forecast (props) {
-  console.log(props.forecastData);
+  console.log(props.forecastData.list);
   return props.isLoading === true
     ? <Loading /> :
     <div>Forecast Component</div>
